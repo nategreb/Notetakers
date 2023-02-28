872efe9f54636a4bdbcb819a7df7274d79fa7e90
Nate Grebelsky <nate.greb@gmail.com>
2023-02-28 10:55:31 -0500

    added more flexibility with the filter to support non-exact matches

--- colleges/filter.py
+++ colleges/filter.py
@@ -7,3 +7,7 @@ class CourseFilter(django_filters.FilterSet):
     class Meta:
         model = Course
         fields = ['course_id', 'course_name', 'department', ]
+    # icontains django function for lookup expressions
+    course_id = django_filters.CharFilter(lookup_expr='icontains')
+    course_name = django_filters.CharFilter(lookup_expr='icontains')
+
